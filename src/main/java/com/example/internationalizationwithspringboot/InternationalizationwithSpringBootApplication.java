1a4b3aa42d247abd1df47b317e66d5b475561751
LongVo248 <vohoanglongfx@gmail.com>
2021-12-17 17:12:13 +0700

    Initial commit

---
+++ src/main/java/com/example/internationalizationwithspringboot/InternationalizationwithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.internationalizationwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalizationwithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalizationwithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/internationalizationwithspringboot/InternationalizationwithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internationalizationwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalizationwithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
